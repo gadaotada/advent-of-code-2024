720396e97f954fdfee613d945c8d8414e3823aff
gadaotada <miro0o92@abv.bg>
2024-12-05 21:32:09 +0200

    day 3 done

--- day-3/index.js
+++ day-3/index.js
@@ -13,6 +13,55 @@ function calculateValidMul(input) {
     }
 
     return sum;
+};
+function parseInstrInp(input, searchStr, firstIdx = 0, occursArr = []) {
+    let currSearchStrIdx = input.indexOf(searchStr);
+
+    if (currSearchStrIdx === -1) {
+        return occursArr;
+    }
+
+    occursArr.push(currSearchStrIdx + firstIdx);
+    let nextInput = input.slice(currSearchStrIdx + searchStr.length);
+    let nextFirstIdx = firstIdx + currSearchStrIdx + searchStr.length;
+    return parseInstrInp(nextInput, searchStr, nextFirstIdx, occursArr);
+}
+
+function findCloseNumber(number, arr) {
+    for (let i = 0; i < arr.length ;i++) {
+        if (arr[i] < number) {
+            continue;
+        } else {
+            return [number, arr[i]]
+        }
+    }
+}
+
+function parseDuples(arr) {
+    let newArr = [];
+    let simNums = [];
+    for (let i = 0; i < arr.length ;i++) {
+        if (i+1 === arr.length) {
+            newArr.push([18989, 19221]);
+            break;
+        }
+
+        if (arr[i][1] !== arr[i+1][1] && simNums.length === 0) {
+            newArr.push(arr[i]);
+        } else if (arr[i][1] === arr[i+1][1] && simNums.length === 0) {
+            simNums.push(arr[i]);
+        } else if (simNums.length > 0) {
+            if (arr[i][1] === simNums[0][1]) {
+                continue;
+            } else {
+                newArr.push(simNums[0]);
+                simNums = [];
+                i--
+            }
+        }
+    }
+
+    return newArr
 }
 
 async function Init() {
@@ -24,7 +73,15 @@ async function Init() {
         console.log("PartOne answer is:", answer);
 
         // ---- PartTwo
-        console.log("PartTwo answer is:", undefined);
+        const dontIdx = parseInstrInp(input, "don't()");
+        const doIdx = parseInstrInp(input, "do()");
+        const firstPart = calculateValidMul(input.slice(0, dontIdx[0]));
+        const duples = doIdx.map(num =>findCloseNumber(num, dontIdx));
+        const filteredDup = parseDuples(duples);
+        const secAnswer = filteredDup.map(el => calculateValidMul(input.slice(el[0], el[1]))).reduce((acc,curr) => acc + curr, 0) + firstPart
+        
+        console.log("PartTwo answer is:",secAnswer);
+
     } catch (err) {
         console.error(err);
     }
